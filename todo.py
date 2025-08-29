fe668b05f610cdf1162da70d464b5fd3584b67c9
Gunnar-Anacker <gunnar.anacker@gmail.com>
2025-08-29 11:26:42 -0500

    new stuff
    new stuff added to python code, including viewing, deleting, and completeing, which is still being revised.

--- todo.py
+++ todo.py
@@ -2,27 +2,32 @@
 
 # Step 1: Start with an empty list to hold tasks
 tasks = []
-
+complete_tasks = []
 # Step 2: Add a task
 def add_task(task):
     tasks.append(task)
 
 # Step 3: View tasks
-
+def view_tasks():
+    for task in tasks:
+        print(task)
+    for task in complete_tasks:
+        print (task)
 # Step 4: Delete a task
-
+def del_task(taskNum):
+    tasks.pop(taskNum)
+            
 
 # Step 5: Mark task complete
-
-
-# Step 6: Save/load tasks (extra stretch for today)
-
-
+def mark_complete(task):
+    completeTask = (tasks[task] + "✅")
+    complete_tasks[task] == (completeTask)
+    
 # Demo flow (you can run this file directly: python todo.py)
 if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
     view_tasks()
+    del_task(1)
     mark_complete(0)
     view_tasks()
-    save_tasks()
